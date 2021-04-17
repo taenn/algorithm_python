c697db3ea87c94a03610335b75ca73594beee796
taenn <>
2021-04-17 06:34:41 +0000

    basic dfs

---
+++ bfs_dfs/dfs.py
@@ -0,0 +1,26 @@
+def dfs(graph, v, visited):
+    # 현재 노드 방문 처리
+    visited[v] = True
+    print(v, end=' ')
+
+    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+
+# 1 ~ 9 노드
+graph = [
+    [],
+    [2, 3, 8],  # 1 과 2, 3, 8 연결
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+visited = [False] * 9
+
+dfs(graph, 1, visited)
